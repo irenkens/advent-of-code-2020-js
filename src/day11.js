c8d1982c399e265dccdbcf67aa75e96383e9973c
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-11 21:17:44 +0100

    refactor: day 11

--- src/day11.js
+++ src/day11.js
@@ -5,7 +5,7 @@ const FLOOR = '.';
 const OCCUPIED = '#';
 const EMPTY = 'L';
 
-const getCell = ({ x, y }, map) => {
+const getCell = (x, y, map) => {
   const outOfBoundsX = x < 0 || x >= map[0].length;
   const outOfBoundsY = y < 0 || y >= map.length;
 
@@ -20,49 +20,47 @@ const getCell = ({ x, y }, map) => {
   return 0;
 };
 
-const getEmptyCell = ({ x, y }, plusX, plusY, map) => {
-  const outOfBoundsX = x < 0 || x >= map[0].length;
-  const outOfBoundsY = y < 0 || y >= map.length;
+const getSeat = (x, y, plusX, plusY, map) => {
+  const posX = x + plusX;
+  const posY = y + plusY;
+  const outOfBoundsX = posX < 0 || posX >= map[0].length;
+  const outOfBoundsY = posY < 0 || posY >= map.length;
 
   if (outOfBoundsX || outOfBoundsY) {
     return 0;
   }
 
-  if (map[y][x] === OCCUPIED) {
+  if (map[posY][posX] === OCCUPIED) {
     return 1;
   }
 
-  if (map[y][x] === FLOOR) {
-    return getEmptyCell({ x: x + plusX, y: y + plusY }, plusX, plusY, map);
+  if (map[posY][posX] === FLOOR) {
+    return getSeat(posX, posY, plusX, plusY, map);
   }
 
   return 0;
 };
 
-const getEmpty = (point, map) => {
-  const { x, y } = point;
-
-  return getEmptyCell({ x: x - 1, y: y - 1 }, -1, -1, map)
-       + getEmptyCell({ x: x - 1, y }, -1, 0, map)
-       + getEmptyCell({ x: x - 1, y: y + 1 }, -1, 1, map)
-       + getEmptyCell({ x, y: y - 1 }, 0, -1, map)
-       + getEmptyCell({ x, y: y + 1 }, 0, 1, map)
-       + getEmptyCell({ x: x + 1, y: y - 1 }, 1, -1, map)
-       + getEmptyCell({ x: x + 1, y }, 1, 0, map)
-       + getEmptyCell({ x: x + 1, y: y + 1 }, 1, 1, map);
+const getVisibleSeats = (x, y, map) => {
+  return getSeat(x, y, -1, -1, map)
+       + getSeat(x, y, -1, 0, map)
+       + getSeat(x, y, -1, 1, map)
+       + getSeat(x, y, 0, -1, map)
+       + getSeat(x, y, 0, 1, map)
+       + getSeat(x, y, 1, -1, map)
+       + getSeat(x, y, 1, 0, map)
+       + getSeat(x, y, 1, 1, map);
 };
 
-const getNeighbors = (point, map) => {
-  const { x, y } = point;
-
-  return getCell({ x: x - 1, y: y - 1 }, map)
-       + getCell({ x: x - 1, y }, map)
-       + getCell({ x: x - 1, y: y + 1 }, map)
-       + getCell({ x, y: y - 1 }, map)
-       + getCell({ x, y: y + 1 }, map)
-       + getCell({ x: x + 1, y: y - 1 }, map)
-       + getCell({ x: x + 1, y }, map)
-       + getCell({ x: x + 1, y: y + 1 }, map);
+const getAdjacentSeats = (x, y, map) => {
+  return getCell(x - 1, y - 1, map)
+       + getCell(x - 1, y, map)
+       + getCell(x - 1, y + 1, map)
+       + getCell(x, y - 1, map)
+       + getCell(x, y + 1, map)
+       + getCell(x + 1, y - 1, map)
+       + getCell(x + 1, y, map)
+       + getCell(x + 1, y + 1, map);
 };
 
 const updateMap = (map, func, maxNeighbors) => {
@@ -70,7 +68,7 @@ const updateMap = (map, func, maxNeighbors) => {
 
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[0].length; x++) {
-      const neighbors = func({ x, y }, map);
+      const neighbors = func(x, y, map);
       if (map[y][x] === EMPTY && neighbors === 0) {
         newMap[y][x] = OCCUPIED;
       }
@@ -81,18 +79,24 @@ const updateMap = (map, func, maxNeighbors) => {
   }
 
   if (isEqual(newMap, map)) {
-    return map.flat().filter(position => position === OCCUPIED).length;
+    return map;
   }
 
   return updateMap(newMap, func, maxNeighbors);
 };
 
+const getOccupiedSeats = map => {
+  return map.flat().filter(seat => seat === OCCUPIED).length;
+};
+
 const part1 = map => {
-  return updateMap(map, getNeighbors, 4);
+  const finalMap = updateMap(map, getAdjacentSeats, 4);
+  return getOccupiedSeats(finalMap);
 };
 
 const part2 = map => {
-  return updateMap(map, getEmpty, 5);
+  const finalMap = updateMap(map, getVisibleSeats, 5);
+  return getOccupiedSeats(finalMap);
 };
 
 export const day11 = async () => {
