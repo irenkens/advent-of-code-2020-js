0b3bd0b5eedf7d5d7ba4b98d86404aa458dafe2e
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-14 23:00:41 +0100

    feat: day 13 part 2

--- src/day13.js
+++ src/day13.js
@@ -1,3 +1,4 @@
+import { xgcd } from 'mathjs';
 import { getInput, splitOnLineBreak, printHeader } from './util';
 
 const part1 = (timestamp, busses) => {
@@ -8,8 +9,32 @@ const part1 = (timestamp, busses) => {
   return inService[busIndex] * minutesToArrival[busIndex];
 };
 
+const getA = (a, n) => {
+  if (a === n) return 0;
+
+  let posA = a;
+  while (posA < 0) {
+    posA += n;
+  }
+  return posA;
+};
+
+// Implementation makes use of Chinese Remainder Theorem
 const part2 = busses => {
-  return 'not implemented';
+  const nums = busses.filter(bus => bus !== 'x').map(bus => +bus);
+  const indices = nums.map(n => busses.indexOf(n.toString()));
+  const remainders = nums.map(num => getA(num - indices[nums.indexOf(num)], num));
+  const N = nums.reduce((total, n) => total * n, 1);
+
+  let sum = BigInt(0);
+  for (let i = 0; i < nums.length; i++) {
+    const a = remainders[i];
+    const y = N / nums[i];
+    const z = xgcd(nums[i], y).get([2]);
+    sum += BigInt(BigInt(a) * BigInt(y) * BigInt(z));
+  }
+
+  return sum % BigInt(N);
 };
 
 export const day13 = async () => {
