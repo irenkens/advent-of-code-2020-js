8030d2912500310b1f9993f70c0f8e7c28d43b9f
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-15 18:25:22 +0100

    feat: day 15

---
+++ src/day15.js
@@ -0,0 +1,42 @@
+import { getInput, printHeader } from './util';
+
+const part1 = numbers => {
+  const spoken = [...numbers];
+
+  for (let i = spoken.length - 1; i < 2020; i++) {
+    const number = spoken[i];
+    const slice = spoken.slice(0, i);
+    const next = slice.includes(number) ? i - slice.lastIndexOf(number) : 0;
+    spoken.push(next);
+  }
+  return spoken[2019];
+};
+
+// faster than part1 but still kinda slow (~5s)
+const part2 = numbers => {
+  const spoken = [...numbers];
+  const lastIndices = new Map();
+  spoken.slice(0, spoken.length - 1).forEach(num => {
+    lastIndices.set(num, spoken.indexOf(num));
+  });
+
+  for (let i = spoken.length - 1; i < 30000000; i++) {
+    const number = spoken[i];
+    const lastIndex = lastIndices.get(number);
+    const next = lastIndices.has(number) ? i - lastIndex : 0;
+    spoken.push(next);
+    lastIndices.set(number, i);
+  }
+
+  return spoken[30000000 - 1];
+};
+
+export const day15 = async () => {
+  const input = await getInput(__filename);
+  const numbers = input.split(',').map(n => +n);
+
+  printHeader(__filename, 1);
+  console.log(part1(numbers));
+  printHeader(__filename, 2);
+  console.log(part2(numbers));
+};
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { day11 } from './day11';
 import { day12 } from './day12';
 import { day13 } from './day13';
 import { day14 } from './day14';
+import { day15 } from './day15';
 
 (async () => {
   await day1();
@@ -30,4 +31,5 @@ import { day14 } from './day14';
   await day12();
   await day13();
   await day14();
+  await day15();
 })();
