fcab5f5fdcd5d51c3e21548292e38cb6573518e3
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-30 16:06:08 +0100

    feat: day 23 part 1

---
+++ src/day23.js
@@ -0,0 +1,42 @@
+import { getInput, printHeader } from './util';
+
+const moveCups = cups => {
+  const currentCup = cups[0];
+  const threeCups = cups.splice(1, 3);
+  let destinationCup = currentCup === 1 ? 9 : currentCup - 1;
+
+  while (cups.indexOf(destinationCup) === -1) {
+    destinationCup = destinationCup === 1 ? 9 : destinationCup - 1;
+  }
+
+  const sliceIndex = cups.indexOf(destinationCup) + 1;
+  const newCups = [...cups.slice(0, sliceIndex), ...threeCups, ...cups.slice(sliceIndex)];
+
+  // shift cups so that the next current cup is at index 0
+  return newCups.concat(newCups.splice(0, 1));
+};
+
+const part1 = cups => {
+  let newCups = [...cups];
+  for (let i = 0; i < 100; i++) {
+    newCups = moveCups(newCups);
+  }
+
+  // shift cups so that cup 1 is at index 0
+  const ordered = newCups.concat(newCups.splice(0, cups.indexOf(1) + 1));
+  return ordered.reduce((result, cup) => `${result}${cup}`, '').slice(1);
+};
+
+const part2 = cups => {
+  return 'not implemented';
+};
+
+export const day23 = async () => {
+  const input = await getInput(__filename);
+  const cups = input.split('').map(cup => +cup);
+
+  printHeader(__filename, 1);
+  console.log(part1(cups));
+  printHeader(__filename, 2);
+  console.log(part2(cups));
+};
--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import { day19 } from './day19';
 import { day20 } from './day20';
 import { day21 } from './day21';
 import { day22 } from './day22';
+import { day23 } from './day23';
 
 (async () => {
   await day1();
@@ -46,4 +47,5 @@ import { day22 } from './day22';
   await day20();
   await day21();
   await day22();
+  await day23();
 })();
