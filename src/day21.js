c7ef30746d2fd5204d1292b08c92df4e2bd28da6
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-30 02:58:29 +0100

    feat: day 21 part 2

--- src/day21.js
+++ src/day21.js
@@ -32,8 +32,51 @@ const part1 = list => {
   return ingredientsPerFood.flat().filter(ingredient => safe.includes(ingredient)).length;
 };
 
+const findMap = (allergensPerFood, ingredientsPerFood, allergens, map = {}) => {
+  const contaminated = [];
+  if (Object.keys(map).length === allergens.length) {
+    return map;
+  }
+
+  let copy = [...ingredientsPerFood];
+  const a = { ...map };
+  allergens.forEach(allergen => {
+    const foods = [];
+    for (let i = 0; i < allergensPerFood.length; i++) {
+      if (allergensPerFood[i].includes(allergen)) {
+        foods.push(ingredientsPerFood[i]);
+      }
+    }
+
+    const risk = intersection(...foods);
+    contaminated.push(risk);
+
+    if (risk.length === 1) {
+      const match = risk[0];
+      a[allergen] = match;
+      copy = [...ingredientsPerFood].map(t => t.filter(f => f !== match));
+    }
+  });
+
+  return findMap(allergensPerFood, copy, allergens, a);
+};
+
 const part2 = list => {
-  return 'not implemented';
+  const ingredientsPerFood = [];
+  const allergensPerFood = [];
+
+  list.forEach(food => {
+    const ingredients = food.split(' (')[0].split(' ');
+    const allergens = food.split('contains ')[1].slice(0, -1).split(', ');
+    ingredientsPerFood.push(ingredients);
+    allergensPerFood.push(allergens);
+  });
+
+  const allergens = Array.from(new Set(allergensPerFood.flat()));
+  const map = findMap(allergensPerFood, ingredientsPerFood, allergens);
+  const solution = [];
+  allergens.sort().map(allergen => solution.push(map[allergen]));
+  return solution.join(',');
 };
 
 export const day21 = async () => {
