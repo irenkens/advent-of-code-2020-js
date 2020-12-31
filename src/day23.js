4876d34cdba7ac562b2dc0f9e39b2027b9f6ca86
irenkens <157256+irenkens@users.noreply.github.com>
2020-12-31 22:51:05 +0100

    feat: day 23 part 2

--- src/day23.js
+++ src/day23.js
@@ -28,8 +28,49 @@ const part1 = cups => {
   return ordered.reduce((result, cup) => `${result}${cup}`, '').slice(1);
 };
 
+// returns a map where the value of a key is the next number in the sequence.
+// e.g. { 1: 5, 2: 9, 5: 7, 7: 2, 9: 1 } = [1, 5, 7, 2, 9]
+const createList = cups => {
+  const list = new Map();
+  for (let i = 0; i < cups.length - 1; i++) {
+    const nr = cups[i];
+    const next = cups[i + 1];
+    list.set(nr, next);
+  }
+  list.set(cups[cups.length - 1], cups[0]);
+  return list;
+};
+
 const part2 = cups => {
-  return 'not implemented';
+  const fullCups = [...cups];
+  for (let i = cups.length + 1; i < 1000001; i++) {
+    fullCups.push(i);
+  }
+
+  const list = createList(fullCups);
+  let current = fullCups[0];
+  const maxCup = 1000000;
+  for (let i = 0; i < 10000000; i++) {
+    const one = list.get(current);
+    const two = list.get(one);
+    const three = list.get(two);
+    const next = list.get(three);
+    let destination = current === 1 ? maxCup : current - 1;
+
+    while (destination === one || destination === two || destination === three) {
+      destination = destination === 1 ? maxCup : destination - 1;
+    }
+    const test = list.get(destination);
+    list.set(current, next);
+    list.set(destination, one);
+    list.set(three, test);
+    current = next;
+  }
+
+  const one = +list.get(1);
+  const two = +list.get(one);
+
+  return one * two;
 };
 
 export const day23 = async () => {
